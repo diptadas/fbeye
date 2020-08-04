04e6e7bdb4a1d1152ebfee3b1716dec6dd2b4013
Nurul_Rafi1 <nurul_rafi1@baylor.edu>
2020-08-04 18:41:41 -0500

    Dummy data shown in newsfeed

--- dummy.js
+++ dummy.js
@@ -1,4 +1,10 @@
 function loadNewsFeed() {
     console.log('loading news feeds');
     console.log(dummyData.feeds[0].text);
+
+    for(var i = 0; i < dummyData.feeds.length; i++) {
+        var newNodeContainer = "<div class='feedObject'><b>" + dummyData.feeds[0].text + "<br><br>";
+        var divClose = "</div>";
+        $('#feed-tab').append(newNodeContainer + divClose);
+      }
 }
