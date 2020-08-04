f82a2a54b2d2de2d12298fb140aa2cc7abc18895
Dipta Das <dipta670@gmail.com>
2020-08-03 22:52:12 -0500

    Separate scripts

--- fbeye.js
+++ fbeye.js
@@ -1,23 +1,45 @@
 $(document).ready(function () {
     console.log("ready");
-
-    var url = 'http://www.texashillcountry.com';
-    var apiUrl = 'https://graph.facebook.com/?ids=' + url;
-
-    $.ajax({
-        url: apiUrl,
-        type: "GET",
-        success: function (result) {
-            console.log(result);
-
-            // $.each(result, function (key, val) {
-            //     console.log(key + ' - ' + val['share']['share_count']);
-            //     var shareCount = val['share']['share_count'];
-            //     console.log(shareCount);
-            // });
-        },
-        error: function (error) {
-            console.log(error);
-        }
-    });
 });
+
+function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+    console.log('statusChangeCallback');
+    console.log(response);                   // The current login status of the person.
+    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+        testAPI();
+    } else {                                 // Not logged into your webpage or we are unable to tell.
+        document.getElementById('status').innerHTML = 'Please log ' +
+            'into this webpage.';
+    }
+}
+
+
+function checkLoginState() {               // Called when a person is finished with the Login Button.
+    FB.getLoginStatus(function (response) {   // See the onlogin handler
+        statusChangeCallback(response);
+    });
+}
+
+
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '926578834514927',
+        cookie: true,                     // Enable cookies to allow the server to access the session.
+        xfbml: true,                     // Parse social plugins on this webpage.
+        version: 'v7.0'           // Use this Graph API version for this call.
+    });
+
+
+    FB.getLoginStatus(function (response) {   // Called after the JS SDK has been initialized.
+        statusChangeCallback(response);        // Returns the login status.
+    });
+};
+
+function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function (response) {
+        console.log('Successful login for: ' + response.name);
+        document.getElementById('status').innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
+    });
+}
