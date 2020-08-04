39b6fcf0521fbc40abcaaf080b4e9a227290a3bc
Dipta Das <dipta670@gmail.com>
2020-08-03 23:07:44 -0500

    Add tabs

--- fbeye.js
+++ fbeye.js
@@ -1,5 +1,32 @@
 $(document).ready(function () {
     console.log("ready");
+    $("#tab1").show();
+    $("#tab2").hide();
+    $("#tab3").hide();
+});
+
+$(document).ready(function () {
+    $("#tab1").show();
+    $("#tab2").hide();
+    $("#tab3").hide();
+});
+
+$("#tab1-btn").click(function () {
+    $("#tab1").show();
+    $("#tab2").hide();
+    $("#tab3").hide();
+});
+
+$("#tab2-btn").click(function () {
+    $("#tab1").hide();
+    $("#tab2").show();
+    $("#tab3").hide();
+});
+
+$("#tab3-btn").click(function () {
+    $("#tab1").hide();
+    $("#tab2").hide();
+    $("#tab3").show();
 });
 
 function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
