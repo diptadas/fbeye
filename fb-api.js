0c46afd35d58ee261eb640cf5fdf209edd1f3917
Dipta Das <dipta670@gmail.com>
2020-08-04 14:58:28 -0500

    Update api version

--- fb-api.js
+++ fb-api.js
@@ -3,7 +3,7 @@ window.fbAsyncInit = function () {
         appId: '926578834514927',
         cookie: true,                     // Enable cookies to allow the server to access the session.
         xfbml: true,                     // Parse social plugins on this webpage.
-        version: 'v7.0'           // Use this Graph API version for this call.
+        version: 'v8.0'           // Use this Graph API version for this call.
     });
 
     FB.getLoginStatus(function (response) {   // Called after the JS SDK has been initialized.
