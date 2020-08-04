450ffa29a31f2a7d44e83ab3b647de69f4deaa0a
Dipta Das <dipta670@gmail.com>
2020-08-04 15:01:15 -0500

    Remove template comments

--- fb-api.js
+++ fb-api.js
@@ -1,17 +1,17 @@
 window.fbAsyncInit = function () {
     FB.init({
         appId: '926578834514927',
-        cookie: true,                     // Enable cookies to allow the server to access the session.
-        xfbml: true,                     // Parse social plugins on this webpage.
-        version: 'v8.0'           // Use this Graph API version for this call.
+        cookie: true,
+        xfbml: true,
+        version: 'v8.0'
     });
 
-    FB.getLoginStatus(function (response) {   // Called after the JS SDK has been initialized.
-        statusChangeCallback(response);        // Returns the login status.
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
     });
 };
 
-function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function (response) {
         console.log('Successful login for: ' + response.name);
@@ -20,21 +20,20 @@ function testAPI() {                      // Testing Graph API after login.  See
     });
 }
 
-function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+function statusChangeCallback(response) {
     console.log('statusChangeCallback');
-    console.log(response);                   // The current login status of the person.
-    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-        $("#login-btn").hide();             // hide login button
+    console.log(response);
+    if (response.status === 'connected') {      // logged in
+        $("#login-btn").hide();                 // hide login button
         testAPI();
-    } else {                                 // Not logged into your webpage or we are unable to tell.
-        $("#login-btn").show();             // show login button
-        document.getElementById('status').innerHTML = 'Please log ' +
-            'into this webpage.';
+    } else {                                    // not logged in
+        $("#login-btn").show();                 // show login button
+        document.getElementById('status').innerHTML = 'Please log into this webpage.';
     }
 }
 
-function checkLoginState() {               // Called when a person is finished with the Login Button.
-    FB.getLoginStatus(function (response) {   // See the onlogin handler
+function checkLoginState() {
+    FB.getLoginStatus(function (response) {
         statusChangeCallback(response);
     });
 }
