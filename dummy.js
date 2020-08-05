321a80169b605b0c5e350807870083869bb0ac88
Nurul_Rafi1 <nurul_rafi1@baylor.edu>
2020-08-04 19:14:26 -0500

    Global variable added for selected tab and running content index of the selected tab

--- dummy.js
+++ dummy.js
@@ -1,10 +1,23 @@
+const tabOptions = {
+    SUMMARY: 1,
+    TIMELINE: 2,
+    NEWSFEED: 3,
+    POST: 4
+}
+var selectedTab = tabOptions.SUMMARY;
+var contentIndexOfIndividualTab = 0;
+
+
 function loadNewsFeed() {
     console.log('loading news feeds');
     console.log(dummyData.feeds[0].text);
 
     for(var i = 0; i < dummyData.feeds.length; i++) {
-        var newNodeContainer = "<div class='feedObject'><b>" + dummyData.feeds[0].text + "<br><br>";
-        var divClose = "</div>";
-        $('#feed-tab').append(newNodeContainer + divClose);
+        var newNodeContainer = "<div class='feedObject'><b>" + dummyData.feeds[i].text + "<br><br>";
+
+        var imageContainer = "<img width=100px height=100px src=\"" + dummyData.feeds[i].image;
+
+        var divClose = "\"</img></div>";
+        $('#feed-tab').append(newNodeContainer + imageContainer + divClose);
       }
 }
