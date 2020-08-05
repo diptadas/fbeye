9254556ad6af18c500c8692bd913161c08bd6c06
Nurul_Rafi1 <nurul_rafi1@baylor.edu>
2020-08-04 20:21:07 -0500

    Read content in newsfeed

--- dummy.js
+++ dummy.js
@@ -7,41 +7,14 @@ const tabOptions = {
 var selectedTab = tabOptions.SUMMARY;
 var contentIndexOfIndividualTab = 0;
 
-
-
-
-
-
-
 function loadNewsFeed() {
+    
     console.log('loading news feeds');
     console.log(dummyData.feeds[0].text);
 
-    /*
     if (contentIndexOfIndividualTab < dummyData.feeds.length) {
-        
-
-        var newNodeContainer = "<div class='feedObject'><b>" + dummyData.feeds[contentIndexOfIndividualTab].text + "<br><br>";
-
-        var imageContainer = "<img class='postImage' width='200px' height='200px' src=\"" + dummyData.feeds[contentIndexOfIndividualTab].image;
-
-        var divClose = "\"</img></div>";
-        $('#feed-tab').append(newNodeContainer + imageContainer + divClose);
-
-        contentIndexOfIndividualTab += 1;
+        readOutFeedContent();
     }
-    */
-
-    if (contentIndexOfIndividualTab < dummyData.feeds.length) {
-        $("#feed-text").text(dummyData.feeds[contentIndexOfIndividualTab].text);
-        $("#feed-image").attr("src",dummyData.feeds[contentIndexOfIndividualTab].image);
-    }
-    
-    //contentIndexOfIndividualTab += 1;
-
-    //contentIndexOfIndividualTab %= dummyData.feeds.length;
-
-    read(dummyData.feeds[contentIndexOfIndividualTab].text);
 }
 
 $(document).keydown(function(e) { 
@@ -52,7 +25,9 @@ $(document).keydown(function(e) {
         console.log("UP");
 
         contentIndexOfIndividualTab -= 1;
-        contentIndexOfIndividualTab < 0 ? 0 : contentIndexOfIndividualTab;
+        if(contentIndexOfIndividualTab < 0) {
+            contentIndexOfIndividualTab = 0;
+        }
         contentIndexOfIndividualTab %= dummyData.feeds.length;
 
         readOutFeedContent();
@@ -72,15 +47,16 @@ $(document).keydown(function(e) {
 });
 
 function readOutFeedContent(){
+
     $("#feed-text").text(dummyData.feeds[contentIndexOfIndividualTab].text);
-        $("#feed-image").attr("src",dummyData.feeds[contentIndexOfIndividualTab].image);
+    $("#feed-image").attr("src",dummyData.feeds[contentIndexOfIndividualTab].image);
 
-        read(dummyData.feeds[contentIndexOfIndividualTab].text);
+    read(dummyData.feeds[contentIndexOfIndividualTab].text);
 
-        var imageContent = "";
-        for(var i = 0; i < dummyData.feeds[contentIndexOfIndividualTab].image-tags.length; i++) {
-            imageContent = imageContent + " " + dummyData.feeds[contentIndexOfIndividualTab].image-tags[i];
-        }
+    var imageContent = "";
+    for(var i = 0; i < dummyData.feeds[contentIndexOfIndividualTab].imageLabels.length; i++) {
+        imageContent = imageContent + " " + dummyData.feeds[contentIndexOfIndividualTab].imageLabels[i];
+    }
 
-        read("There is an image here which contains " + imageContent);
+    read("There is an image here which contains " + imageContent);
 }
