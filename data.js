98faaaa93d0288987eb7b640a6c719fb4ae168b8
Nurul_Rafi1 <nurul_rafi1@baylor.edu>
2020-08-04 20:09:47 -0500

    read out content added

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ dummyData =
       "id": 1,
       "time": "2020-07-25T04:34:06+0000",
       "name": "David Jones",
-      "text": "The red ball sat proudly at the top of the toybox. It had been the last to be played with and anticipated it would be the next as well. The other toys grumbled beneath. At one time each had held the spot of the red ball, but over time they had sunk deeper and deeper into the toy box.",
+      "text": "The red ball sat proudly at the top of the toybox.",
       "image": "https://picsum.photos/id/1011/500",
       "location": "Waco, TX",
       "tags": [
@@ -38,7 +38,7 @@ dummyData =
       "id": 2,
       "time": "2020-05-25T04:34:06+0000",
       "name": "Michael Johnson",
-      "text": "The chair sat in the corner where it had been for over 25 years. The only difference was there was someone actually sitting in it. How long had it been since someone had done that? Ten years or more he imagined. Yet there was no denying the presence in the chair now.",
+      "text": "The chair sat in the corner where it had been for over 25 years.",
       "image": "https://picsum.photos/id/1011/106",
       "location": "Austin, TX",
       "tags": [
--- dummy.js
+++ dummy.js
@@ -8,6 +8,11 @@ var selectedTab = tabOptions.SUMMARY;
 var contentIndexOfIndividualTab = 0;
 
 
+
+
+
+
+
 function loadNewsFeed() {
     console.log('loading news feeds');
     console.log(dummyData.feeds[0].text);
@@ -27,11 +32,55 @@ function loadNewsFeed() {
     }
     */
 
+    if (contentIndexOfIndividualTab < dummyData.feeds.length) {
+        $("#feed-text").text(dummyData.feeds[contentIndexOfIndividualTab].text);
+        $("#feed-image").attr("src",dummyData.feeds[contentIndexOfIndividualTab].image);
+    }
+    
+    //contentIndexOfIndividualTab += 1;
 
+    //contentIndexOfIndividualTab %= dummyData.feeds.length;
 
+    read(dummyData.feeds[contentIndexOfIndividualTab].text);
+}
+
+$(document).keydown(function(e) { 
+
+    console.log(selectedTab);
+
+    if (e.keyCode == '38') {
+        console.log("UP");
+
+        contentIndexOfIndividualTab -= 1;
+        contentIndexOfIndividualTab < 0 ? 0 : contentIndexOfIndividualTab;
+        contentIndexOfIndividualTab %= dummyData.feeds.length;
+
+        readOutFeedContent();
+
+    }
+
+    else if (e.keyCode == '40') {
+        
+        console.log("DOWN");
+
+        contentIndexOfIndividualTab += 1;
+        contentIndexOfIndividualTab %= dummyData.feeds.length;
+
+        readOutFeedContent();
+        
+    }
+});
+
+function readOutFeedContent(){
     $("#feed-text").text(dummyData.feeds[contentIndexOfIndividualTab].text);
-    $("#feed-image").attr("src",dummyData.feeds[contentIndexOfIndividualTab].image);
-    contentIndexOfIndividualTab += 1;
+        $("#feed-image").attr("src",dummyData.feeds[contentIndexOfIndividualTab].image);
 
-    contentIndexOfIndividualTab %= dummyData.feeds.length;
+        read(dummyData.feeds[contentIndexOfIndividualTab].text);
+
+        var imageContent = "";
+        for(var i = 0; i < dummyData.feeds[contentIndexOfIndividualTab].image-tags.length; i++) {
+            imageContent = imageContent + " " + dummyData.feeds[contentIndexOfIndividualTab].image-tags[i];
+        }
+
+        read("There is an image here which contains " + imageContent);
 }
